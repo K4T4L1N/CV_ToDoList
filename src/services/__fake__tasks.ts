ca1f212e4d56bed684ce717403dc7412d0466943
K4T4L1N <catlin.hackman@gmail.com>
2023-11-06 21:56:03 +0200

    for test purpose

--- src/services/__fake__tasks.ts
+++ src/services/__fake__tasks.ts
@@ -1,25 +1,27 @@
 export const TasksList = [
-/**
-   * If filter("All" ,"Complete" or "Active") is selected or searchTerm entered,
-   * filteredTasks or searchedTasks are shown;
-   * Otherwise, activeTasks render.
+
      {
     id: "asdf123",
-    title: "Learn React.js",
+    title: "ToDoApp React.js",
+    date: "2023-11-01",
+    done: true,
+  },
+  {
+    id: "asdf355",
+    title: "Complete ToDoApp React.js",
     date: "2023-11-01",
     done: true,
   },
   {
     id: "asdf456",
-    title: "Pay the bills",
-    date: "2023-11-06",
+    title: "Deploy the app somewhere",
+    date: "2023-11-28",
     done: false,
   },
   {
     id: "asdf789",
-    title: "Start new project",
+    title: "?Start new project?",
     date: "2023-11-19",
     done: false,
   },
-  */
 ];
